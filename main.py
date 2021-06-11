2e468451e8ada520db547d0ceec94ee48399e87b
tchubey <63860283+tchubey@users.noreply.github.com>
2021-06-11 18:31:46 +0200

    Remove deprecated files

--- main.py
+++ main.py
@@ -5,31 +5,23 @@ from scripts.utils import Collection
 
 from scripts.submit import Run, handle_args
 
-from mytools.ConcreteModel_25 import ConcreteModel
+from mytools.JointModel import ConcreteModel
 from mytools.misc import config_seeds
 def main(tasks):
     if not tasks:
         warnings.warn("The run will have no effect since no tasks were given.")
         return
-    config_seeds()
+    config_seeds(seed = 0)
     
-    name = "bert_test_edit"
-    model = ConcreteModel(model_name = name)
+    name = "model_8"
+
+    model = ConcreteModel(model_name = name, config_path="config/model_params.json")
     
-    model.train(finput_train = Path("data/example"))#, output_name='bert_apartideotromodelo', from_scratch = False)#, finput_valid = Path("data/ex/"))
-    #model.train(finput_train = Path("data/training/"), finput_valid = Path("data/development/main"))
+    model.train(finput_train = Path("data/training/"), finput_valid = Path("data/development/main"))
+   
+    Run.submit(name, tasks, model)
     
-    finput = Path("data/example2/")
-    collection2 = (
-        Collection().load_dir(finput)
-        if finput.is_dir()
-        else Collection().load(finput)
-    )
-    
-    model.run(collection2, taskA=True, taskB=True)
-        
-    #Run.submit(name, tasks, model)
-    
+
 
 if __name__ == "__main__":
     import logging
